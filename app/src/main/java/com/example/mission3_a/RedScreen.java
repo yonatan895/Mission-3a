842b3ec8a438e5293f0296e3d283eb304ec96aaa
jony <jony04y01@gmail.com>
2022-08-18 13:19:46 +0300

    Final Commit

---
+++ app/src/main/java/com/example/mission3_a/RedScreen.java
@@ -0,0 +1,14 @@
+package com.example.mission3_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RedScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red_screen);
+    }
+}
